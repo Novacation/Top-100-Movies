3f76dc76da00e72f3efcfd2f4dc907fd08686f3c
Celestino <andersoncelestino.n@gmail.com>
2021-12-28 15:02:04 -0300

    Update ApiToken.ts

--- app/Models/ApiToken.ts
+++ app/Models/ApiToken.ts
@@ -5,6 +5,18 @@ export default class ApiToken extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public user_id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public type: string
+
+  @column()
+  public token: string
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
